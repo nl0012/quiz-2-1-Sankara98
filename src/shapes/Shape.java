927e283fc81e753e1e183f1ded564b5797170ac3
nl0012 <nl0012@csee137aer18.lcsee.wvu.edu>
2017-10-09 15:43:17 -0400

    did Shape class

--- src/shapes/Shape.java
+++ src/shapes/Shape.java
@@ -1,16 +1,17 @@
 package shapes;
 
 public abstract class Shape implements Comparable<Shape> {
-	/*
-	 * Implement compareTo such that shapes are compared by their areas. Follow
-	 * the canonical expectations of compareTo interface. See Java API for
-	 * expected behavior.
-	 * 
-	 * Also you need to finish implementing the Square and Circle classes.
-	 */
-	public int compareTo(Shape shape) {
-		// TODO Implement me
+	
+
+	public final int compareTo(Shape shape) {
+		if (shape.calculateArea() > this.calculateArea())
+			return 1;
+		else if (shape.calculateArea() == this.calculateArea())
+			return 0;
+		
+		else return -1;
 	}
 
 	public abstract double calculateArea();
+
 }
